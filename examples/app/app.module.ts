e66c3b427cd016d5ec2828314ae238d005f6e101
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-04-14 00:29:52 +0200

    chore: Make it pretty.

--- examples/app/app.module.ts
+++ examples/app/app.module.ts
@@ -1,20 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
