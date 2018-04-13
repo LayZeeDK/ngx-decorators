2b841e7ce138a267f04c6f1521eb48b760cc54f1
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-04-14 00:56:45 +0200

    chore: Make it pretty.

--- examples/app/app.component.ts
+++ examples/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'example-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'example';
+  public title = 'example';
 }
