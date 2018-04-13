9781a4a4d8e57fcb9276ca37ced3d5f137e4da4c
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-04-13 22:51:49 +0200

    Rearrange library to prepare for packages.

--- src/libs/component-configuration/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './change-detection';
-export * from './component-options-combinator';
-export * from './component.decorator';
-export * from './host-element';
-export * from './view-encapsulation';
