6c1823d1a9cfaf7ee3d0b3067a685d2c7c6958cd
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-05-10 01:27:02 +0200

    test: fix unit tests

--- projects/ngx-decorators/src/test.ts
+++ projects/ngx-decorators/src/test.ts
@@ -2,6 +2,8 @@
 // and framework files
 // tslint:disable:ordered-imports
 
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
