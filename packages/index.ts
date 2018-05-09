1c95d1ead1b9c9bad3ac3b8c85ffe32314c3a432
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-05-09 23:48:05 +0200

    chore: remove packages directory

--- packages/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/change-detection';
-export * from './src/component-options-combinator';
-export * from './src/component.decorator';
-export * from './src/host-element';
-export * from './src/view-encapsulation';
