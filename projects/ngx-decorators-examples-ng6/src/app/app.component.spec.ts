07782fc9507e3dd4e5b2b7d0f77a8b589b7381b0
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-05-14 21:42:34 +0200

    test(examples): fix app component spec

--- projects/ngx-decorators-examples-ng6/src/app/app.component.spec.ts
+++ projects/ngx-decorators-examples-ng6/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -6,12 +7,9 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-      ],
-      declarations: [
-        AppComponent,
-      ],
+      declarations: [AppComponent],
+      imports: [RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -24,7 +22,7 @@ describe('AppComponent', () => {
     const fixture: ComponentFixture<AppComponent> =
       TestBed.createComponent(AppComponent);
     const app: AppComponent = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('example');
+    expect(app.title).toEqual('Decorators for Angular Version 6 Examples');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture: ComponentFixture<AppComponent> =
